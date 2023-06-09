278bc53f0a9638655b3c4b3eab8a6f173912f609
natty98282 <natty98282@gmail.com>
2023-06-09 11:26:06 +0300

    api-change

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,27 +1,36 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  Future<String> getdata() async {
+    var url = 'https://jsonplaceholder.typicode.com/todos/1';
+    var response = await http.get(Uri.parse(url));
+    var data = jsonEncode(response.body);
+    return data;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
-          color: Colors.blue.shade300,
-        ),
-        child: const Center(
-          child: Text(
-            'Hello Dwell am Coming',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
-            ),
-          ),
-        ),
-      ),
-    );
+        appBar: AppBar(),
+        body: FutureBuilder<String>(
+          future: getdata(),
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.hasData) {
+              return Center(
+                child: Text(snapshot.data.toString()),
+              );
+            } else {
+              return const Center(
+                child: Text('loading...'),
+              );
+            }
+          },
+        ));
   }
 }
